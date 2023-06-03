da9ca284d19762b6f43b1146e9e3941e9f4992fd
Sangeetha2125 <gsangeetha7113@gmail.com>
2023-06-03 10:16:05 +0530

    Merged All Code Changes

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ let navbar=document.querySelector('.navbar-container');
 menubtn.onclick=()=>{
     navbar.classList.toggle('open-menu');
 }
+
+let scrollContainer = document.querySelector(".gallery");
+let backbtn = document.getElementById("backbtn");
+let nextbtn = document.getElementById("nextbtn");
+
+scrollContainer.addEventListener("wheel", (evt) =>{
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    scrollContainer.style.scrollBehaviour = "auto";
+});
+
+nextbtn.addEventListener("click", ()=> {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 417;
+});
+
+backbtn.addEventListener("click", ()=> {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 417;
+});
