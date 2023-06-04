ff45559157765e769d3daf4bfdea4b858a7535f4
Sangeetha2125 <gsangeetha7113@gmail.com>
2023-06-04 13:36:43 +0530

    Pricelist Section Added

--- js/glider.js
+++
@@ -1,8 +0,0 @@
-new Glider(document.querySelector('.glider'), {
-    slidesToShow: 3,
-    dots: '#dots',
-    arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
-    }
-  });
--- js/script.js
+++ js/script.js
@@ -24,3 +24,33 @@ backbtn.addEventListener("click", ()=> {
     scrollContainer.style.scrollBehavior = "smooth";
     scrollContainer.scrollLeft -= 417;
 });
+
+let contactbtn1=document.getElementById('contact-btn-1');
+let contactbtn2=document.getElementById('contact-btn-2');
+let closebtn=document.getElementById('close-form');
+
+contactbtn1.onclick=()=>{
+    document.getElementById('form-popup').style.display="block";
+}
+contactbtn2.onclick=()=>{
+    document.getElementById('form-popup').style.display="block";
+}
+closebtn.onclick=()=>{
+    document.getElementById('form-popup').style.display="none";
+}
+
+let contactform=document.getElementById('form-popup');
+let name1=document.getElementById('name1');
+let email1=document.getElementById('email1');
+let mobile1=document.getElementById('mobile1');
+let comment1=document.getElementById('comment1');
+window.onclick = function(event){
+    if (event.target == contactform)
+    {
+        contactform.style.display = "none";
+        name1.value='';
+        email1.value='';
+        mobile1.value='';
+        comment1.value='';
+    }
+}
